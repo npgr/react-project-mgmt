07e63bb86207b9eced40cb84417ea97af82a58a0
Nuno Goncalves <ngocalves@gmail.com>
2018-07-24 19:14:19 +0200

    include redux-promise-middleware in store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 // import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './js/reducers/index.js';
 
 // const initialState = {}
 const enhancers = [];
 const middleware = [
+  promise(),
   thunk
 ];
 
