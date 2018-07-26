14e5d0d8d294580b1793957cd21d59bcd6927cfe
Nuno Goncalves <ngocalves@gmail.com>
2018-07-26 22:07:47 +0200

    RAML Mocker for /user/credentials

--- resources/formats.js
+++ resources/formats.js
@@ -1,7 +1,11 @@
 exports.formats = {
   // personName: faker => faker.name.findName(),
   companyName: faker => faker.company.companyName(),
+  email: faker => faker.internet.email(),
   longInteger: faker => faker.random.number({min:10000, max:90000}),
+  personName: faker => `${faker.name.firstName()} ${faker.name.lastName()}`,
   projectName: faker => 'Project ' + faker.company.companyName(),
-  projectStatus: faker => faker.random.arrayElement(['active','inactive'])
+  projectStatus: faker => faker.random.arrayElement(['active','inactive']),
+  user: faker => faker.internet.userName(),
+  userRol: faker => faker.random.arrayElement(['admin', 'Team Leader', 'Developer'])
 };
