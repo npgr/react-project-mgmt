32b2ec7bf7766e756a5a08a400ba4509123225d1
Nuno Goncalves <ngocalves@gmail.com>
2018-08-06 17:40:13 +0200

    include tests for: AppNavigation, AppNotifications

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,9 +4,10 @@ import Enzyme, {/* shallow, mount, render */ } from 'enzyme';
 import { expect } from 'chai';
 import { /*mountWithIntl,*/ shallowWithIntl, loadTranslation } from 'enzyme-react-intl';
 import App from '../js/containers/App.jsx';
-import AppHeader from '../js/containers/AppHeader.jsx';
+import AppNavigation from '../js/containers/AppNavigation.jsx';
+import AppNotifications from '../js/containers/AppNotifications.jsx';
+//import CreateProject from '../js/containers/CreateProject/CreateProject.jsx';
 import store from '../store';
-import { setAppTitle } from '../js/actions/configuration';
 // import chaiEnzyme from 'chai-enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -23,17 +24,36 @@ describe('<App />', () => {
   it('it has a object property intl.messages', () => {
     expect(component.props().intl).to.have.property('messages').to.be.a('object');
   });
+  // it('Render <AppNavigation /> inside <App />', () => {
+  //   //console.log(component.dive());
+  //   expect(component.dive().find(AppNavigation)).to.have.length(1);
+  // });
+  // it('Render <CreateProject /> inside <App />', () => {
+  //   expect(component.dive().find(CreateProject)).to.have.length(1);
+  // });
 });
 
-describe('<AppHeader />', () => {
+describe('<AppNavigation />', () => {
   let component = null;
-  it('It has a string property title = Mi Titulo', () => {
+  it('renders 1 <AppNavigation /> component', () => {
+    component = shallowWithIntl(<AppNavigation store={store} />);
+    expect(component).to.have.length(1);
+  });
+  /*it('It has a string property title = Mi Titulo', () => {
     store.dispatch(setAppTitle('Mi Titulo2'));
-    component = shallowWithIntl(<AppHeader store={store} />);
+    component = shallowWithIntl(<AppNavigation store={store} />);
     expect(component.props()).to.have.property('title').to.be.a('string').equals('Mi Titulo2');
   });
   it('Render title = Mi Titulo2', () => {
     expect(component.dive().find('.app-title').render().text()).equals('Mi Titulo2');
+  });*/
+});
+
+describe('<AppNotifications />', () => {
+  let component = null;
+  it('renders 1 <AppNotifications /> component', () => {
+    component = shallowWithIntl(<AppNotifications store={store} />);
+    expect(component).to.have.length(1);
   });
 });
 
